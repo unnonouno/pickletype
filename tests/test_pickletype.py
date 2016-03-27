260c7c10fae01317e3585e7db201ec7ee9b27252
Yuya Unno <unnonouno@gmail.com>
2016-03-27 23:11:53 +0900

    Add test for List and Dict

--- tests/test_pickletype.py
+++ tests/test_pickletype.py
@@ -38,3 +38,26 @@ class TestPickleType(unittest.TestCase):
     def test_dict(self):
         x = {'x': 1, 'y': 2}
         self.assertEqual(pt.get_type(x), pt.Dict(str, int))
+
+
+class TestList(unittest.TestCase):
+
+    def setUp(self):
+        self.typ = pt.List(int)
+
+    def test_eq(self):
+        self.assertEqual(self.typ, pt.List(int))
+        self.assertNotEqual(self.typ, pt.List(str))
+        self.assertNotEqual(self.typ, int)
+
+
+class TestDict(unittest.TestCase):
+
+    def setUp(self):
+        self.typ = pt.Dict(str, int)
+
+    def test_eq(self):
+        self.assertEqual(self.typ, pt.Dict(str, int))
+        self.assertNotEqual(self.typ, pt.Dict(str, str))
+        self.assertNotEqual(self.typ, pt.Dict(int, int))
+        self.assertNotEqual(self.typ, int)
